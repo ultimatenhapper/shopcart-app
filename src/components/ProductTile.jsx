82172f0c2d58f524b9f089593beac56284c33498
Javi <belikemike3057@hotmail.com>
2024-03-15 13:14:44 +0100

    Cart and redux store implementation

--- src/components/ProductTile.jsx
+++ src/components/ProductTile.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../store/slices/cart-slice";
 
 function ProductTile({ product }) {
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state) => state);
+
+  function handleAddToCart() {
+    dispatch(addToCart(product));
+  }
+
+  function handleRemoveFromCart() {
+    dispatch(removeFromCart(product.id));
+  }
+
   return (
     <div>
       <div className="flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
@@ -17,8 +30,17 @@ function ProductTile({ product }) {
           </h1>
         </div>
         <div className="flex items-center justify-center w-full mt-5">
-          <button className="bg-red-950 text-white border-2 rounded-lg font-bold p-4">
-            Add to cart
+          <button
+            onClick={
+              cart.some((item) => item.id === product.id)
+                ? handleRemoveFromCart
+                : handleAddToCart
+            }
+            className="bg-red-950 text-white border-2 rounded-lg font-bold p-4"
+          >
+            {cart.some((item) => item.id === product.id)
+              ? "Remove from cart"
+              : "Add to cart"}
           </button>
         </div>
       </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CartTile from "../components/CartTile";
 
 function Cart() {
-  return <div>Cart</div>;
+  const [totalCart, setTotalCart] = useState(0);
+  const { cart } = useSelector((state) => state);
+
+  useEffect(() => {
+    setTotalCart(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
+
+  console.log(cart, totalCart);
+
+  return (
+    <div className="flex justify-center">
+      {cart && cart.length ? (
+        <>
+          <div className="min-h-[80vh] grid md:grid-cols-2 max-w-6xl mx-auto">
+            <div className="flex flex-col justify-center items-center p-3">
+              {cart.map((cartItem) => (
+                <CartTile cartItem={cartItem} />
+              ))}
+            </div>
+          </div>
+          <div className="w-[300px]">
+            <div className="flex flex-col justify-center items-end p-5 space-y-5 mt-14">
+              <h1 className="font-bold text-lg text-red-800">
+                Your cart summary
+              </h1>
+              <p>
+                <span className="text-gray-800 font-bold">Total items</span>
+                <span>: {cart.length}</span>
+              </p>
+              <p>
+                <span className="text-gray-800 font-bold">Total amount</span>
+                <span>: {totalCart}</span>
+              </p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="min-h-[80vh] flex flex-col items-center justify-center">
+          <h1 className="text-gray-800 font-bold text-xl mb-2">
+            Your car is empty
+          </h1>
+          <Link to={"/"}>
+            <button className="bg-red-950 text-white border-2 rounded-lg font-bold p-4">
+              SHOP NOW
+            </button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Cart;
